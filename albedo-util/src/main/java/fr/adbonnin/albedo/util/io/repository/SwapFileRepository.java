4cee73fdb2dd103c603a5df84c5063856efd2128
adbonnin <adbonnin@users.noreply.github.com>
2016-07-15 21:57:00 +0200

    feat(io) : Ajout de SwapFileRepository

---
+++ albedo-util/src/main/java/fr/adbonnin/albedo/util/io/repository/SwapFileRepository.java
@@ -0,0 +1,23 @@
+package fr.adbonnin.albedo.util.io.repository;
+
+import fr.adbonnin.albedo.util.io.serializer.EntitySerializer;
+
+import java.io.File;
+import java.lang.reflect.Type;
+
+import static java.util.Objects.requireNonNull;
+
+public class SwapFileRepository {
+
+    private final File file;
+
+    private final Type type;
+
+    private final EntitySerializer serializer;
+
+    public SwapFileRepository(File file, Type type, EntitySerializer serializer) {
+        this.file = requireNonNull(file);
+        this.type = requireNonNull(type);
+        this.serializer = requireNonNull(serializer);
+    }
+}
